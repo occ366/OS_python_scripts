29dfdb6fb72ba4b4a05a92de2dd59c50ab8a6153
yethi <cuellas@gmail.com>
2021-08-17 08:21:36 +0200

    Create week2.py
    Create a couple of functions:
    
    frist function open files in txt  format from one path and build a json
    second function open and send info to one server
    third funcion main, take control of the flow of the program.

---
+++ week2.py
@@ -0,0 +1,14 @@
+
+
+def fuction load_json_from_file(path):
+  """ Read lines of one file and crate a json """
+  pass
+
+def api_call(url,json):
+  """ Call the api of one server by url and upload the info of one json """
+  pass
+
+def main():
+  """ main functio: call other fuction for each file we need"""
+  pass
+

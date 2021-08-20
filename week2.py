81d32d7d17d19e8fbc30cc62304f25188de8ea33
yethi <cuellas@gmail.com>
2021-08-20 10:30:12 +0200

    Update week2.py
    Change some error an conf settings

--- week2.py
+++ week2.py
@@ -7,35 +7,39 @@ from requests import ConnectionError
 from requests import HTTPError
 from requests import Timeout
 
-json_model={"title":0, "name":1, "date":2, "feedback":3}	 
+json_model={"title":0, "name":1, "date":2, "feedback":3}
 
-def load_json_from_file(file):
+def load_json_from_file(path,file):
   """ Read lines of one file and crate a json """
   json ={}
-  with open(file) as f:
+  with open(os.path.join(path,file)) as f:
     json_txt=f.readlines()
-    for field,position in json_model-items():
-      json[field]=json_txt[position]
+    for field,position in json_model.items():
+      json[field.strip()]=json_txt[position].strip()
   f.close()
+  print(json)
   return json
 
-def api_call(url,json):
+def api_call(url,Json):
   """ Call the api of one server by url and upload the info of one json """
+  print("try to uploader in {}, json: {} ".format(url,Json))
   try:
-    set=requests.post(url,data=json)
+    set=requests.post(url,data=json.dumps(Json))
     if set.status_code is "201":
       print(set.text)
     else:
       set.raise_for_status()
-    
+
   except:
-    print("Unexpected error "+ str(set))
+    print("Unexpected error"+str(set))
+
 
 def main():
   """ main functio: call other fuction for each file we need"""
-  path ="./"
-  url="http:://"+sys.arg[1]+"/feedback"
+  path ="/data/feedback/"
+  url="http://"+str(sys.argv[1])+"/feedback"
   for file in os.listdir(path):
-    json=load_json_from_file(file)
+    json=load_json_from_file(path,file)
     api_call(url,json)
 
+main()

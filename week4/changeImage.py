fc7c35564780905045c620359c9af1c8c6224347
yethi <cuellas@gmail.com>
2021-08-27 15:38:58 +0200

    Update changeImage.py

--- week4/changeImage.py
+++ week4/changeImage.py
@@ -11,12 +11,11 @@ def convert_tiff_jpeg(dpath,opath):
    if re.search(pattern,img):
       with Image.open(os.path.join(opath,img)) as im:
         if im.format in "TIFF":
-          conv=im.resize((600,400))
-          conv=conv.convert("RGB")
-          conv.save(os.path.join(dpath,(img+'.jpeg')),'JPEG')
+          im.resize((600,400)).convert("RGB").save(os.path.join(dpath,(img+'.jpeg')),'JPEG')
+
    im.close() 
 
- def main():
+if __name__ == "__main__":
   USER = os.getenv('USER')
   path = '/home/{}/supplier-data/images/'.format(USER)
   convert_tiff_jpeg(path,path):

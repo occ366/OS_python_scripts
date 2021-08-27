50e169b309a4a2b190ea9a2a0e5ad495c9887f3c
yethi <cuellas@gmail.com>
2021-08-27 22:26:32 +0200

    Update report_email.py

--- week4/report_email.py
+++ week4/report_email.py
@@ -8,23 +8,25 @@ import datetime
 import emails
 
 def paragraph(path):
-  parafraph=[]
+  parag=''
   for file in os.listdir(path):
-    data=run.load_load_json_from_file(path,file)
-    paragraph.append("name: {} \n weight: {} lbs".format(data[""name],data[weight]))
+    data=run.load_json_from_file(path,file)
+    parag=parag+("name: {}\nweight: {}\n\n".format(data["name"],data["weight"]))
 
-  return paragrapgh
+  return parag
 
 if __name__ == "__main__":
   USER = os.getenv('USER')
-  path = '/home/{}/supplier-data/images/'.format(USER)
-  attachment =‘/tmp/processed.pdf'
+  path = '/home/{}/supplier-data/descriptions/'.format(USER)
+  attachment ='/tmp/processed.pdf'
   title = 'Process update on'+ str(datetime.date.today().strftime("%B %d, %Y"))
-  reports.generate_report(attachment, title, '<br/>'.join(paragraph(path)))
+  print(paragraph(path))
+  reports.generate_report(attachment, title, paragraph(path))
   message=emails.generate_email("automation@example.com",
                                 "{}@example.com".format(USER),
                                 "Upload Completed - Online Fruit Store",
                                 "All fruits are uploaded to our website successfully. A detailed list is attached to this email.",
                                 attachment)
   email.send_email(message)
+
   

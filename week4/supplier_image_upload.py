95c8b09f6b6550a42a8ef3a8edc255c7f2e95d4a
yethi <cuellas@gmail.com>
2021-08-27 21:31:57 +0200

    Update supplier_image_upload.py
    work !!!!

--- week4/supplier_image_upload.py
+++ week4/supplier_image_upload.py
@@ -2,20 +2,23 @@
 
 import requests
 import os
+import re
 
 def upload_images(path,url):
-  for file in os os.listdir(path):
-    with open(os.path.join(path,file), 'rb') as image_to_upload:
-      try:
-        u_imag=requests.post(url,files={'file':image_to_upload})
-      if set.status_code is "201":
-        print(u_imag.status_code)
-      else:
-        set.raise_for_status()
-    except:
-      print("Unexpected error"+str(u_imag))
-      
- if __name__ == "__main__":
+
+  for file in os.listdir(path):
+    if re.search(r'jpeg',file):
+      with open(os.path.join(path,file), 'rb') as image_to_upload:
+        try:
+          u_imag=requests.post(url,files={'file':image_to_upload})
+          if u_imag.status_code is "201":
+            print(u_imag.status_code)
+          else:
+            set.raise_for_status()
+        except:
+          print("Unexpected error"+str(u_imag))
+
+if __name__ == "__main__":
 
    url='http://localhost/upload/'
    path='/home/{}/supplier-data/images/'.format(os.getenv('USER'))

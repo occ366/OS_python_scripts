725a1b9ef5bb278260b38c8fbf42cb251022095a
yethi <cuellas@gmail.com>
2021-08-27 22:03:13 +0200

    Update run.py

--- week4/run.py
+++ week4/run.py
@@ -4,6 +4,7 @@ import os
 import json
 import requests
 import sys
+import re
 
 def load_json_from_file(path,file):
   """ Read lines of one file and crate a json """
@@ -13,31 +14,32 @@ def load_json_from_file(path,file):
     Json_txt=f.readlines()
     for field,position in json_model.items():
       Json[field.strip()]=Json_txt[position].strip('\n')
+    Json['image_name']=re.sub('txt','jpeg',file)
   f.close()
-  return json.dumps(Json)
+  return Json
 
 def api_call(url,Json):
   """ Call the api of one server by url and upload the info of one json """
   headers = {'Content-type': 'application/json'}
   try:
     payload={'json_payload': Json}
-    set=requests.post(url,data=Json,headers=headers)
+    set=requests.post(url,data=Json),headers=headers)
     if set.status_code is "201":
       print(set.status_code)
     else:
       set.raise_for_status()
   except:
-    print("Unexpected error"+str(set))
+    print("Unexpected error"+str(set.status_code))
 
 if __name__ == "__main__":
 
   """ main functio: call other fuction for each file we need"""
-  ipadd=sys.agrv[1]
-  path ="path='/home/{}/supplier-data/descriptions/'.format(os.getenv('USER'))
-  url="http://{}/fruits/?format=api"´.format(ipadd)
+  ipadd=sys.argv[1]
+  path ='/home/{}/supplier-data/descriptions/'.format(os.getenv('USER'))
+  url="http://{}/fruits/".format(ipadd)
   for file in os.listdir(path):
-    json=load_json_from_file(path,file)
-    json[weight]=int(re.search(r'([\d]*)',json[weight])[0])
-    json['image_name']=re.sub['.txt', '.jpeg',file]
-    api_call(url,json)
-
+    Json=load_json_from_file(path,file)
+    w_string = Json["weight"]
+    w_int =re.search(r'([\d]*)',w_string)
+    Json["weight"]=int(w_int[0])
+    api_call(url,json.dumps(Json))

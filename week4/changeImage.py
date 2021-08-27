f6db4c1c31f3877966b6f6a25cac17803753bdc7
yethi <cuellas@gmail.com>
2021-08-27 14:03:04 +0200

    Update changeImage.py

--- week4/changeImage.py
+++ week4/changeImage.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python3
+import re
+import os
+from PIL import Image
 
+
+def convert_tiff_jpeg(dpath,opath):
+  """convert tiff image to jpge""""
+  files = os.listdir(opath)
+  for img in files:
+   if re.search(pattern,img):
+      with Image.open(os.path.join(opath,img)) as im:
+        if im.format in "TIFF":
+          conv=im.resize((600,400))
+          conv=conv.convert("RGB")
+          conv.save(os.path.join(dpath,(img+'.jpeg')),'JPEG')
+   im.close() 
+
+ def main():
+  USER = os.getenv('USER')
+  path = '/home/{}/supplier-data/images/'.format(USER)
+  convert_tiff_jpeg(path,path):
+      
+  
+main()

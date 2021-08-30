3394f9d8464d13851884f0f10460176663788dcc
yethi <cuellas@gmail.com>
2021-08-30 12:05:06 +0200

    Update reports.py

--- week4/reports.py
+++ week4/reports.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-
 from reportlab.platypus import SimpleDocTemplate
-from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.platypus import Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib import colors
 
-def generate_report(attachment, title, paragraph):
-  styles = getSampleStyleSheet()
-  report = SimpleDocTemplate(filename)
-  report_title = Paragraph(title, styles["h1"])
-  report_info = Paragraph(additional_info, styles["BodyText"])
-  empty_line = Spacer(1,20)
-  report.build([report_title, empty_line, report_info, empty_line])
+def generate_report(filename, title, info):
+    styles = getSampleStyleSheet()
+    report = SimpleDocTemplate(filename)
+    report_title = Paragraph(title, styles["h1"])
+    report_info = Paragraph(info, styles["BodyText"])
+    empty_line = Spacer(1,20)
+
+    report.build([report_title, empty_line, report_info, empty_line])

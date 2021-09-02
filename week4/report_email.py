ca4e860eff1982d94730968486df8a0def4545ed
yethi <cuellas@gmail.com>
2021-09-02 08:10:13 +0200

    Update report_email.py
    Change paragrafh(), to build a good format, and the way a invoque email  building

--- week4/report_email.py
+++ week4/report_email.py
@@ -8,24 +8,24 @@ import datetime
 import emails
 
 def paragraph(path):
-  parag=''
+  parag=[]
   for file in os.listdir(path):
     data=run.load_json_from_file(path,file)
-    parag=parag+("name: {}\nweight: {}\n\n".format(data["name"],data["weight"]))
+    parag=parag+["name: {}".format(data["name"])]+["weight: {}".format(data["weight"])]+["  "]
 
-  return parag
+  return '<br/>'.join(parag)
 
 if __name__ == "__main__":
   USER = os.getenv('USER')
   path = '/home/{}/supplier-data/descriptions/'.format(USER)
   attachment ='/tmp/processed.pdf'
-  title = 'Process update on'+ str(datetime.date.today().strftime("%B %d, %Y"))
+  title = 'Process Update on '+ str(datetime.date.today().strftime("%B %d, %Y"))
   reports.generate_report(attachment, title, paragraph(path))
-  message=emails.generate_email("automation@example.com",
+  message=emails.email_generate("automation@example.com",
                                 "{}@example.com".format(USER),
                                 "Upload Completed - Online Fruit Store",
                                 "All fruits are uploaded to our website successfully. A detailed list is attached to this email.",
                                 attachment)
-  email.send_email(message)
+  emails.email_send(message)
 
   
